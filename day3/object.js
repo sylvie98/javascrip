035af25d4c4779c9d61051c6c3d8e77e116397c8
sylvie98 <sylviemasengesho5@gmail.com>
2024-02-28 10:54:27 +0200

    this today i learn nested object

--- day3/object.js
+++ day3/object.js
@@ -6,3 +6,16 @@ person.age = 50;
 person.eyeColor = "blue";
 
 console.log(person);
+// to day i learn nested object
+const object = {
+    class:{
+        women:20,
+        teacher: 3
+    },
+    shecan: {
+        peaple:60,
+        trainee :10
+    }
+};
+console.log(object.class.teacher);
+console.log(object.shecan.trainee);
