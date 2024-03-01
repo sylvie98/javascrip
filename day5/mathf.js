848721384a66a0ca6a2ab29acf51b122bebb1c54
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-01 09:15:23 +0200

    to i learn math function

---
+++ day5/mathf.js
@@ -0,0 +1,9 @@
+//abs help to change negative numbe into positive number
+let num=-10;
+(console.log(Math.abs(num)));
+//ceil help to change decimal number into whole number
+let numb=3.4;
+(console.log(Math.ceil(numb)));//ceil any decimal you have high or low increment 1
+(console.log(Math.round(numb)));
+(console.log(Math.floor(numb)));// 
+(console.log(Math.random(numb)));
