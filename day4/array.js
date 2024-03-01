fb68c559791617862f6e6010bb7d3b8f562cfb7e
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-01 09:09:28 +0200

    to day i learn loop in array

--- day4/array.js
+++ day4/array.js
@@ -1,4 +1,27 @@
 //to day i learn  nested array
-const arr= [[1,2],[3,4],[5]];
+const arr= [[1,2],[3,4],[5,6]];
 console.log(arr[2]);
+
+//start learn array and function
+let array = ["orange","blue","green"];
+console.log(array);
+//insert element 
+array.push("yellow");
+console.log(array);
+//remove element from last one
+array.pop();
+console.log(array);
+//checked if any colors include in colors array
+let check= array.includes("orange");
+console.log(check);
+//using loop in array
+for (let i = 0; i < 2; i++) {
+    console.log(array[i]);
+  }
+  let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = numbers.map(num => num * 2);
+console.log(numbers);
+console.log(doubledNumbers);
+
+
 
