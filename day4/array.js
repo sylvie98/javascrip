c51c8a0c1d4d2f477b9bd63cc0da75aaa9d597a0
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-01 10:59:00 +0200

    today i try to contiue learn array

--- day4/array.js
+++ day4/array.js
@@ -14,6 +14,10 @@ console.log(array);
 //checked if any colors include in colors array
 let check= array.includes("orange");
 console.log(check);
+//using for each
+array.forEach(function(array) {
+  console.log(array);
+});
 //using loop in array
 for (let i = 0; i < 2; i++) {
     console.log(array[i]);
@@ -22,6 +26,16 @@ for (let i = 0; i < 2; i++) {
 let doubledNumbers = numbers.map(num => num * 2);
 console.log(numbers);
 console.log(doubledNumbers);
-
-
-
+//replace blue with orange
+let replaced = array.map(array => array === "blue" ? "orange" : array);
+console.log(replaced);
+//convert object into array
+let userObject = { id: 1, name: "John", age: 25 };
+let userArray = Object.entries(userObject);
+console.log(userObject);
+console.log(userArray );
+//convert array into object
+let keyArray = [["name", "Alice"], ["age", 30], ["city", "New York"]];
+let reconstructedObject = Object.fromEntries(keyArray);
+console.log(keyArray);
+console.log(reconstructedObject);
